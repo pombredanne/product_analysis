40e9a029fab65572aa769a94eb0aefee1da6d2ea
Roverzon <Roverzon@appledeMacBook-Pro.local>
2017-04-11 14:42:21 +0800

    fill 0 in behavioral data after being merged by user_sim data

--- reader.py
+++ reader.py
@@ -6,13 +6,6 @@ from simulator import user_simulator
 from datetime import datetime
 
 
-def to_secs( duration_string=None ):
-    if duration_string == "0":
-        return 0
-    else:
-        return int(duration_string.split("分")[0])*60 + int(duration_string.split("分")[1].split("秒")[0])
-
-
 
 user_data_sim_df = pd.read_csv("./user_data_all_sim.txt",dtype={"user_id" : str, "year" : np.int64, "week_num":np.int64})
 
@@ -31,8 +24,6 @@ user_org_info_df = pd.read_csv(file_names["user_org_info"],dtype="str")
 
 session_name = ["Date","user","duration_min","visits"]
 user_sessions_df = pd.read_csv("./1201-0330 user_访问量&访问时长.csv",dtype={"user":str},parse_dates=parse_dates,names=session_name,header=0)
-# user_sessions_df["year"] = user_sessions_df["Date"].dt.year
-# user_sessions_df["week_num"] = user_sessions_df["Date"].dt.week
 
 
 view_name = ["Date","user","chart_all_pv","chart_list_pv","create_chart_pv","edit_chart_pv",
@@ -60,19 +51,32 @@ user_behavior_click_df = pd.read_csv(file_names["user_behavior_click"],sep="\t",
 dfs = [user_sessions_df, user_behavior_view_df, user_behavior_click_df]
 user_behaviors = reduce(lambda left, right: pd.merge(left,right,how="left", on=["user","Date"]),dfs).fillna(0)
 
-users_sim = user_simulator("2017/1/1",datetime.now(),period=1,user_file="user_join_org_info_raw.csv")
+user_behaviors.to_csv("user_behavior" + datetime.date() + ".csv")
+
+users_sim = user_simulator("2017/1/1",datetime.now(),period=1,file_name="user_join_org_info_raw.csv")
+
+users_sim_org = pd.merge(users_sim,user_org_info_df,how="inner",on="user_id")
 
 
-result = pd.merge(users_sim,user_behaviors,how="left", left_on=["user_id","sim_date"],right_on=["user","Date"] )
+result = pd.merge(users_sim_org,user_behaviors,how="left", left_on=["user_id","sim_date"],right_on=["user","Date"] )[["chart_all_pv","chart_list_pv","create_chart_pv","edit_chart_pv",
+                  "dashboard_all_pv","create_dashboard_pv","edit_dashboard_pv","dashboard_list_pv",
+                  "funnel_all_pv","retention_all_pv","user_details_pv","realtime_pv","heatmap_use_imp",
+                  "create_funnel_pv","dashboard_filter_clck","dashboard_usercohort_clck",
+                      "dashboard_time_clck","dashboard_create_save_clck",
+                      "dashboard_edit_update_clck",
+                      "chart_detail_usercohort_clck","chart_detail_filter_clck",
+                      "chart_detail_time_clck","chart_create_save_clck",
+                      "chart_edit_savetoanother_clck","chart_edit_update_clck",
+                      "chart_list_filter_clck","chart_list_time_clck",
+                      "funnel_Dimension_clck","funnel_time_clck",
+                      "funnel_trend_clck","funnel_create_save_clck",
+                      "retention_time_clck","retention_Dimension_clck",
+                      "retention_detail_behavior_clck","visits"]].fillna(0)
 
 
+result.to_csv("product_analytics_raw_data.csv")
 
-# # print(result.columns.values)
-user_behaviors.to_csv("user_behaviors.csv")
-result.to_csv("result.csv")
-#
-# # print(result)
-#
+
 print("done")
 
 
