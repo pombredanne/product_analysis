a66c7252a5f7e2049c6793344fa3f2f472776992
Roverzon <Roverzon@appledeMacBook-Pro.local>
2017-04-11 12:23:16 +0800

    refactor simulator.py and integrated in reader.py

--- reader.py
+++ reader.py
@@ -2,7 +2,18 @@
 import pandas as pd
 import numpy as np 
 from functools import reduce
-#
+from simulator import user_simulator
+from datetime import datetime
+
+
+def to_secs( duration_string=None ):
+    if duration_string == "0":
+        return 0
+    else:
+        return int(duration_string.split("分")[0])*60 + int(duration_string.split("分")[1].split("秒")[0])
+
+
+
 user_data_sim_df = pd.read_csv("./user_data_all_sim.txt",dtype={"user_id" : str, "year" : np.int64, "week_num":np.int64})
 
 file_names = {
@@ -12,36 +23,56 @@ file_names = {
     "user_behavior_click"   : "1228-0327 FQY_主要功能数据_U_user_table_action交互类.txt"
 }
 
-parse_dates = ['时间']
+parse_dates = ['Date']
+
 
 user_org_info_df = pd.read_csv(file_names["user_org_info"],dtype="str")
 # user_sessions_df  = pd.read_csv(file_names["user_session"],encoding="utf-16",sep="\t",dtype={"user" : str, "访问时长 (分钟)" : str, "访问量":np.int64}, parse_dates=parse_dates)
-user_sessions_df = pd.read_csv("./1201-0330 user_访问量&访问时长.csv",dtype="str",parse_dates=parse_dates)
-user_sessions_df["year"] = user_sessions_df["时间"].dt.year
-user_sessions_df["week_num"] = user_sessions_df["时间"].dt.week
+
+session_name = ["Date","user","duration_min","visits"]
+user_sessions_df = pd.read_csv("./1201-0330 user_访问量&访问时长.csv",dtype={"user":str},parse_dates=parse_dates,names=session_name,header=0)
+# user_sessions_df["year"] = user_sessions_df["Date"].dt.year
+# user_sessions_df["week_num"] = user_sessions_df["Date"].dt.week
 
 
-user_behavior_view_df = pd.read_csv(file_names["user_behavior_view"],encoding="utf-8",sep="\t",dtype={'user':str, '主要功能——单图细节（分析师专用勿改）_页面浏览量':np.int64, 'fqy-单图列表页面_页面浏览量':np.int64,
- 'fqy-创建单图页面_页面浏览量' : np.int64, '单图-编辑页面(分析师专用勿改)_页面浏览量' : np.int64, '看板全部页面（分析师专用勿改）_页面浏览量' :np.int64,
- 'fqy-创建看板页面_页面浏览量':np.int64, '看板-编辑页面(分析师专用勿动)_页面浏览量' : np.int64, 'fqy-看板列表页面(勿动)_页面浏览量' : np.int64,
- '主要功能——漏斗全部_页面浏览量':np.int64, '主要功能——留存全部页面_页面浏览量':np.int64, '主要功能——用户细查_页面浏览量' : np.int64,
- '主要功能——实时_页面浏览量':np.int64, '热图使用情况用这个指标_浏览量':np.int64, '新建——漏斗_页面浏览量':np.int64},parse_dates=parse_dates)
+view_name = ["Date","user","chart_all_pv","chart_list_pv","create_chart_pv","edit_chart_pv",
+                  "dashboard_all_pv","create_dashboard_pv","edit_dashboard_pv","dashboard_list_pv",
+                  "funnel_all_pv","retention_all_pv","user_details_pv","realtime_pv","heatmap_use_imp",
+                  "create_funnel_pv"]
 
-user_behavior_click_df = pd.read_csv(file_names["user_behavior_click"],sep="\t",encoding="utf-8",dtype={'user':str, '看板-套用筛选条件控件(分析师专用勿改)_点击量':np.int64, '看板-套用分群控件(分析师专用勿改)_点击量':np.int64,
- '看板-自定义时间页卡(分析师专用勿改)_点击量':np.int64, '看板-创建看板-保存按钮(分析师专用勿动)_点击量':np.int64,
- 'ZSQ-编辑看板-更新按钮(分析师专用勿改)_点击量':np.int64, '单图-详情页-用户分群过滤按钮(分析师专用勿改)_点击量':np.int64,
- '单图-详情页-数据过滤按钮(分析师专用勿改)_点击量':np.int64, '单图-详情页-时间控件按钮(分析师专用勿改)_点击量':np.int64,
- 'fqy-创建单图-保存_点击量' '单图——另存(分析师专用勿改)_点击量' :np.int64,'ZSQ-单图编辑页面-更新按钮(分析师专用勿改)_点击量':np.int64,
- '单图-列表页条件筛选控件(分析师专用勿改)_点击量':np.int64, '单图-列表页时间控件(分析师专用勿改)_点击量':np.int64,
- '可视化分析操作 - 漏斗分析 - 維度選擇_点击量':np.int64, 'zjl-漏斗页面-切换时间_点击量':np.int64,
- '可视化分析操作 - 漏斗分析 - 漏斗趨勢_点击量':np.int64, '漏斗——保存_点击量' '留存-留存率时间切换（分析师用勿改）_点击量':np.int64,
- '留存-选择留存切分维度/分群_点击量':np.int64, 'zyw-留存2.0-选择功能/起始行为/回访行为_点击量':np.int64 }, parse_dates=['时间'])
+user_behavior_view_df = pd.read_csv(file_names["user_behavior_view"],encoding="utf-8",sep="\t",dtype={'user':str},parse_dates=parse_dates,names=view_name,header=0)
+
+click_name = ["Date","user","dashboard_filter_clck","dashboard_usercohort_clck",
+                      "dashboard_time_clck","dashboard_create_save_clck",
+                      "dashboard_edit_update_clck",
+                      "chart_detail_usercohort_clck","chart_detail_filter_clck",
+                      "chart_detail_time_clck","chart_create_save_clck",
+                      "chart_edit_savetoanother_clck","chart_edit_update_clck",
+                      "chart_list_filter_clck","chart_list_time_clck",
+                      "funnel_Dimension_clck","funnel_time_clck",
+                      "funnel_trend_clck","funnel_create_save_clck",
+                      "retention_time_clck","retention_Dimension_clck",
+                      "retention_detail_behavior_clck"]
+
+user_behavior_click_df = pd.read_csv(file_names["user_behavior_click"],sep="\t",encoding="utf-8",dtype={'user':str}, parse_dates=parse_dates,names=click_name,header=0)
 
 
 dfs = [user_sessions_df, user_behavior_view_df, user_behavior_click_df]
-behaviors = reduce(lambda left, right: pd.merge(left,right,how="left", on=["user","时间"]),dfs)
+user_behaviors = reduce(lambda left, right: pd.merge(left,right,how="left", on=["user","Date"]),dfs).fillna(0)
 
-tmp4 = pd.merge(user_data_sim_df,behaviors,how="left",left_on=["user_id","year","week_num"],right_on=["user","year","week_num"])
-result = pd.merge(tmp4,user_org_info_df,how="inner",on="user_id")
-print(len(result))
+users_sim = user_simulator("2017/1/1",datetime.now(),period=1,user_file="user_join_org_info_raw.csv")
+
+
+result = pd.merge(users_sim,user_behaviors,how="left", left_on=["user_id","sim_date"],right_on=["user","Date"] )
+
+
+
+# # print(result.columns.values)
+user_behaviors.to_csv("user_behaviors.csv")
+result.to_csv("result.csv")
+#
+# # print(result)
+#
+print("done")
+
 
--- simulator.py
+++ simulator.py
@@ -4,7 +4,6 @@ from dateutil import parser
 from itertools import product
 from datetime import datetime
 
-
 def days_generator(start_date=None,end_date=None,period=1):
     start = parser.parse(start_date)
     end   = parser.parse(end_date)
@@ -14,9 +13,9 @@ def week_number_convertor(tseries):
     return [ time.isocalendar()[1] for time in tseries ]
 
 def user_id_generator(start_user_id=1,end_user_id=None):
-    return [ user_id for user_id in np.arange(start_user_id,end_user_id+ 1) ]
+    return [ str(user_id) for user_id in np.arange(start_user_id,end_user_id+ 1) ]
 
-def user_simulator(start_date=None,end_date=None,period=None):
+def user_simulator(start_date=None,end_date=None,period=None,user_file=None):
 
     if type(end_date) != str or type(start_date) != str :
         end_date   = str(end_date)
@@ -24,12 +23,23 @@ def user_simulator(start_date=None,end_date=None,period=None):
     else:
         pass
 
-    users_iter = product(user_id_generator(1,60000),days_generator(start_date,end_date,period))
+    users = [ user_id for user_id in product(user_id_generator(1,60000),days_generator(start_date,end_date,period)) ]
+    users_df = pd.DataFrame(users,columns=["user_id","sim_date"])
 
-    return [ user for user in users_iter ]
+    user_info_df = pd.read_csv("user_join_org_info_raw.csv", encoding="utf-8",
+                            parse_dates=["min_day_user_join_org", "update_date"], dtype={"user_id":str}).drop_duplicates("user_id")
+
+    result = pd.merge(users_df, user_info_df, how="left", on="user_id")
+
+    return result.loc[result["sim_date"] >= result["min_day_user_join_org"]]
 
 
 if __name__ == "__main__":
 
-    users = user_simulator("2016/1/1",datetime.now(),7)
-    users_df = pd.DataFrame(users,columns=["user_id","date"])
+    users = user_simulator("2017/1/1",datetime.now(),period=1,user_file="user_join_org_info_raw.csv")
+
+    # print(users.loc[users["user_id"] == "51160"])
+
+    # users.to_csv("sim_users-201-1-1-2017-4-11.csv")
+    #
+    # print("done")
