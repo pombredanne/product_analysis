ac70785be2bb18f8d4c2bf46724fec4c9638a459
Roverzon <Roverzon@appledeMacBook-Pro.local>
2017-04-11 14:41:13 +0800

    add output to csv file

--- simulator.py
+++ simulator.py
@@ -15,7 +15,7 @@ def week_number_convertor(tseries):
 def user_id_generator(start_user_id=1,end_user_id=None):
     return [ str(user_id) for user_id in np.arange(start_user_id,end_user_id+ 1) ]
 
-def user_simulator(start_date=None,end_date=None,period=None,user_file=None):
+def user_simulator(start_date=None,end_date=None,period=None,file_name=None):
 
     if type(end_date) != str or type(start_date) != str :
         end_date   = str(end_date)
@@ -23,10 +23,10 @@ def user_simulator(start_date=None,end_date=None,period=None,user_file=None):
     else:
         pass
 
-    users = [ user_id for user_id in product(user_id_generator(1,60000),days_generator(start_date,end_date,period)) ]
-    users_df = pd.DataFrame(users,columns=["user_id","sim_date"])
+    users_df = pd.DataFrame([ user_id for user_id in product(user_id_generator(1,60000),days_generator(start_date,end_date,period)) ],
+                            columns=["user_id","sim_date"])
 
-    user_info_df = pd.read_csv("user_join_org_info_raw.csv", encoding="utf-8",
+    user_info_df = pd.read_csv(file_name, encoding="utf-8",
                             parse_dates=["min_day_user_join_org", "update_date"], dtype={"user_id":str}).drop_duplicates("user_id")
 
     result = pd.merge(users_df, user_info_df, how="left", on="user_id")
@@ -36,10 +36,9 @@ def user_simulator(start_date=None,end_date=None,period=None,user_file=None):
 
 if __name__ == "__main__":
 
-    users = user_simulator("2017/1/1",datetime.now(),period=1,user_file="user_join_org_info_raw.csv")
+    start_date = "2017/1/1"
+    end_date   = datetime.now()
 
-    # print(users.loc[users["user_id"] == "51160"])
+    users = user_simulator(start_date,end_date,period=1,file_name="user_join_org_info_raw.csv")
+    users.to_csv("user_sim_" + "-".join(start_date.split("/")) + "_" + str(end_date.year) +"-" + str(end_date.month) + "-"+ str(end_date.day) + ".csv")
 
-    # users.to_csv("sim_users-201-1-1-2017-4-11.csv")
-    #
-    # print("done")
