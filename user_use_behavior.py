a3fd5a6b5ce9cc43785511441fb54aa076ea77ca
Roverzon <Roverzon@appledeMacBook-Pro.local>
2017-04-12 11:24:30 +0800

    add secs convertor in avg_duration

--- user_use_behavior.py
+++ user_use_behavior.py
@@ -4,10 +4,11 @@ from functools import reduce
 from simulator import user_simulator
 from datetime import datetime
 import time
+import  re
 
 
 
-session_behavior = ["duration_min","visits"]
+session_behavior = ["avg_duration","visits"]
 view_event = ["chart_all_pv","chart_list_pv","create_chart_pv","edit_chart_pv",
                   "dashboard_all_pv","create_dashboard_pv","edit_dashboard_pv","dashboard_list_pv",
                   "funnel_all_pv","retention_all_pv","user_details_pv","realtime_pv","heatmap_use_imp",
@@ -27,6 +28,19 @@ click_event = ["dashboard_filter_clck","dashboard_usercohort_clck",
 
 behavior_names = [session_behavior,view_event,click_event]
 
+def secs_convertor(time=None):
+    try:
+        time_array = re.compile("(.*?)分(.*?)秒").findall(str(time))
+    except:
+        print(time)
+
+    if len(time_array) > 0:
+        result = int(time_array[0][0]) * 60 + int(time_array[0][1])
+    else:
+        result = 0
+
+    return result
+
 def behavior_data_generator(files=[],key=[]):
 
     if len(files) == 0:
@@ -38,12 +52,15 @@ def behavior_data_generator(files=[],key=[]):
             df_names =  key + behavior_names[file_i]
             dfs.append(pd.read_csv(files[file_i], encoding="utf-16", sep="\t", dtype="str",names=df_names,header=0,parse_dates=['Date']))
 
-        return reduce(lambda left, right: pd.merge(left, right, how="left", on=["user", "Date"]), dfs).fillna(0)
+    result = reduce(lambda left, right: pd.merge(left, right, how="left", on=["user", "Date"]), dfs).fillna(0)
+    result["avg_duration"] = [ secs_convertor(d) for d in result["avg_duration"] ]
+
+    return result
 
 
 def user_generator(sim_user_filter=None,user_org_filter=None):
 
-    users_sim = user_simulator("2017/1/1", datetime.now(), period=1, file_name=sim_user_filter)
+    users_sim = user_simulator("2017/1/1","2017/4/9", period=1, file_name=sim_user_filter)
     user_org_info_df = pd.read_csv(user_org_filter, dtype="str")
 
     return pd.merge(users_sim, user_org_info_df, how="inner", on="user_id")
@@ -51,9 +68,11 @@ def user_generator(sim_user_filter=None,user_org_filter=None):
 
 
 if __name__ == "__main__":
-    gio_files = ["./exame_sample/1201-0330 user_访问量&访问时长.csv","./fqy_sample/1228-0327 FQY_主要功能数据_U_user_table_PV浏览类.csv","./fqy_sample/1228-0327 FQY_主要功能数据_U_user_table_action交互类.csv"]
+    gio_files = ["./0412/0101-0409 user_访问量&访问时长.csv","./0412/0101-0409 FQY_主要功能数据_U_user_table_PV浏览类.csv","./0412/0101-0409 FQY_主要功能数据_U_user_table_action交互类.csv"]
 
     behavioral_data = behavior_data_generator(files=gio_files,key=["Date","user"])
+
+
     users = user_generator(sim_user_filter="user_join_org_info_raw.csv",user_org_filter="user_org_info.txt")
 
     result = pd.merge(users,behavioral_data, how="left", left_on=["user_id","sim_date"],right_on=["user","Date"])
@@ -61,7 +80,14 @@ if __name__ == "__main__":
     columns = session_behavior + view_event + click_event
     result[columns] = result[columns].fillna(0)
 
+
+    # print(result["avg_duration"])
+
     print(len(result))
+    #
+    # behavioral_data.to_csv("./0412_result/user_behavior.csv")
+    # users.to_csv("./0412_result/user_sim.csv")
+    # result.to_csv("./0412_result/result.csv")
 
 
 
