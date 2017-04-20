f4947ab88c05c52feb9fae28ca77ebb7137668eb
Roverzon <Roverzon@appledeMacBook-Pro.local>
2017-04-20 18:58:28 +0800

    encapsulate user type selection in function

--- user_use_behavior.py
+++ user_use_behavior.py
@@ -5,9 +5,6 @@ from simulator import user_simulator
 import  re
 import time
 
-
-
-
 session_behavior = ["avg_duration","visits"]
 view_event = ["chart_all_pv","chart_list_pv","create_chart_pv","edit_chart_pv",
                   "dashboard_all_pv","create_dashboard_pv","edit_dashboard_pv","dashboard_list_pv",
@@ -24,9 +21,6 @@ click_event = ["dashboard_filter_clck","dashboard_usercohort_clck",
                       "funnel_trend_clck","funnel_create_save_clck",
                       "retention_time_clck","retention_Dimension_clck",
                       "retention_detail_behavior_clck"]
-
-
-
 interactive_expl_sum_key = ["user_details_pv","retention_detail_behavior_clck",
 "funnel_time_clck","funnel_trend_clck","funnel_Dimension_clck",
 "chart_list_time_clck","chart_list_filter_clck","chart_detail_filter_clck",
@@ -35,7 +29,7 @@ interactive_expl_sum_key = ["user_details_pv","retention_detail_behavior_clck",
 "retention_time_clck","retention_Dimension_clck",
 "dashboard_usercohort_clck","dashboard_filter_clck","dashboard_time_clck"]
 
-consumption =  ["create_chart_pv","create_dashboard_pv","edit_chart_pv","create_funnel_pv","edit_dashboard_pv"]
+computed_fields = ["consumption_pv_sum","interactive_action_sum","single_diagram_view","funnel_report_view","analytics_dashboard","visual_analytic"]
 
 
 behavior_names = [session_behavior,view_event,click_event]
@@ -65,17 +59,14 @@ def behavior_data_generator(files=[],key=[]):
             df_names =  key + behavior_names[file_i]
             dfs.append(pd.read_csv(files[file_i], encoding="utf-16", sep="\t", dtype={"user":str},names=df_names,header=0,parse_dates=['Date'],infer_datetime_format=True))
 
-    result = reduce(lambda left, right: pd.merge(left, right, how="left", on=["user", "Date"]), dfs).fillna(0)
+    result = reduce(lambda left, right: pd.merge(left, right, how="left", on=["user", "Date"]), dfs)
+    result = result.fillna(0)
+
     result["avg_duration"] = result["avg_duration"].map(lambda time : secs_convertor(time))
 
-
-
     result["consumption_pv_sum"] = result["create_chart_pv"] + result["create_dashboard_pv"] + result["edit_chart_pv"] + result["create_funnel_pv"] + result["edit_dashboard_pv"]
-
     result["interactive_action_sum"] = result["user_details_pv"]+result["retention_detail_behavior_clck"]+result["funnel_time_clck"]+result["funnel_trend_clck"]+result["funnel_Dimension_clck"]+result["chart_list_time_clck"]+result["chart_list_filter_clck"]+result["chart_detail_filter_clck"]+result["chart_detail_time_clck"]+result["chart_detail_usercohort_clck"]+result["heatmap_use_imp"]+result["retention_time_clck"]+result["retention_Dimension_clck"]+result["dashboard_usercohort_clck"]+result["dashboard_filter_clck"]+result["dashboard_time_clck"]
-
     result["single_diagram_view"] = result["chart_all_pv"] - result["chart_list_pv"] - result["create_chart_pv"]-result["edit_chart_pv"]
-
     result["funnel_report_view"] = result["funnel_all_pv"] - result["create_funnel_pv"]
 
     result["analytics_dashboard"] = result["dashboard_all_pv"]-result["realtime_pv"]-result["create_dashboard_pv"]-result["edit_dashboard_pv"]-result["dashboard_list_pv"]
@@ -119,12 +110,21 @@ def cohort_analysis(periods=None, sample=None, init_behavior=None,return_behavio
         cohorts_per.append(overlap_per)
 
     if not number:
-        results = ( cohorts_per,  cohorts_user )
+        cohorts_obj = ( cohorts_per,  cohorts_user )
     else:
-        results = ( cohorts_num , cohorts_user )
+        cohorts_obj = ( cohorts_num , cohorts_user )
 
-    return results
+    return cohorts_obj
 
+
+def get_core_user(sample=pd.DataFrame):
+    return sample[(sample["visual_analytic"] > 0) & (sample["interactive_action_sum"] > 0)  & (sample["consumption_pv_sum"] > 0 )]
+
+def get_active_user(sample=pd.DataFrame):
+    return sample[(sample["visual_analytic"] > 0) & (sample["interactive_action_sum"] > 0)  | (sample["consumption_pv_sum"] > 0 )]
+
+def get_casual_user(sample=pd.DataFrame):
+    return sample[(sample["visual_analytic"] > 0) & (sample["interactive_action_sum"] == 0)  & (sample["consumption_pv_sum"] == 0 )]
 
 
 if __name__ == "__main__":
@@ -135,43 +135,42 @@ if __name__ == "__main__":
     behavioral_data = behavior_data_generator(files=gio_files,key=["Date","user"])
     users = user_generator(sim_user_filter="user_join_org_info_raw.csv",user_org_filter="user_org_info.txt")
 
-
-    columns = session_behavior + view_event + click_event
+    columns = session_behavior + view_event + click_event + computed_fields
     result = pd.merge(users,behavioral_data, how="left", left_on=["user_id","sim_date"],right_on=["user","Date"])
     result[columns] = result[columns].fillna(0)
 
 
-    core_user   = result[(result["visual_analytic"] > 0) & (result["interactive_action_sum"] > 0)  & (result["consumption_pv_sum"] > 0 )]
-    active_user = result[(result["visual_analytic"] > 0) & (result["interactive_action_sum"] > 0)  | (result["consumption_pv_sum"] > 0)]
-    casual_user = result[(result["visual_analytic"] > 0) & (result["interactive_action_sum"] == 0) | (result["consumption_pv_sum"] == 0)]
+    core_user   = get_core_user(result)
+    active_user = get_active_user(result)
+    casual_user = get_casual_user(result)
 
 
 
     print("DON'T BE PANICK. DATA ARE PREPARED")
 
-    # WN = 11
-    # i = 0
-    #
-    # for user in [ core_user , active_user, casual_user ]:
-    #
-    #     if i == 0:
-    #         print("Core User Retention Report")
-    #     elif i == 1:
-    #         print("Active User Retention Report")
-    #     else:
-    #         print("Casual User Retention Report")
-    #
-    #     report_per , report_users =  cohort_analysis(periods=WN,sample=user,number=False)
-    #
-    #     for line in report_per:
-    #         s = ""
-    #         for j in line:
-    #             s += str(j) + "% "
-    #         print(s)
-    #
-    #     i += 1
-    #
-    #     print("\n")
+    WN = 11
+    i = 0
+
+    for user in [ core_user , active_user, casual_user ]:
+
+        if i == 0:
+            print("Core User Retention Report")
+        elif i == 1:
+            print("Active User Retention Report")
+        else:
+            print("Casual User Retention Report")
+
+        report_per , report_users =  cohort_analysis(periods=WN,sample=user,number=False)
+
+        for line in report_per:
+            s = ""
+            for j in line:
+                s += str(j) + "% "
+            print(s)
+
+        i += 1
+
+        print("\n")
 
 
     # behavioral_data.to_csv("./0412_result/user_behavior.csv")
