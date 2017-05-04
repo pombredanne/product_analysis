97ee56b1b139bab0f07eb93e095016c82523e1d2
rov01 <roverzon@gmail.com>
2017-05-04 19:44:03 +0800

    calculate user in march

--- sdk_install.py
+++ sdk_install.py
@@ -1,81 +1,30 @@
 import pandas as pd
 import numpy as np
-
-
-def sdk_page_source_analysis(df=pd.DataFrame):
-
-    newdf = df.groupby(["org_name"])["SDK_platform_view", "SDK_complete_click"].agg([np.sum])
-    newdf = newdf.assign(install_rate=newdf["SDK_complete_click"] / newdf["SDK_platform_view"])
-
-    qualified_account = newdf[newdf.install_rate > 0].reset_index()
-    qualified_account.columns = ["org_name", "SDK_platform_view", "SDK_complete_click", "install_rate"]
-    qualified_account_info = pd.merge(df, qualified_account, how="inner", on=["org_name"])
-    qualified_account_info.loc[:, "page_source_cat"] = [page.split("/")[-1] for page in qualified_account_info["page_source"]]
-
-    qualified_account_info_pc = qualified_account_info.groupby("page_source_cat")["page_source_cat"].agg("count")
-    print(qualified_account_info_pc.sort_values(ascending=False))
-
-    qualified_account_by_industry = qualified_account_info.groupby(["industry"])["org_name"].agg("count")
-    print(qualified_account_by_industry.sort_values(ascending=False))
-
-    non_qualified_accounts = newdf[newdf.install_rate == 0].reset_index()
-    non_qualified_accounts.columns = ["org_name", "SDK_platform_view", "SDK_complete_click", "install_rate"]
-    non_qualified_accounts_info = pd.merge(df, non_qualified_accounts, how="inner", on=["org_name"])
-
-    non_qualified_accounts_by_industry = non_qualified_accounts_info.groupby(["industry"])["org_name"].agg("count")
-    print(non_qualified_accounts_by_industry.sort_values(ascending=False))
-
-
-
-
+import math
 
 
 def sdk_company_analysis(df=pd.DataFrame, org_in_db=pd.DataFrame):
 
-    df = df.groupby(["org_name"])[["user", "page_source", "SDK_platform_view", "SDK_complete_click"]].agg([len, np.sum]).reset_index()
-    df["install_rate"] = df["SDK_complete_click"]["sum"] / df["SDK_platform_view"]["sum"]
+    nqa_proj = df[(df.first_date_of_getting_pv.isnull()) & (df["SDK_complete_click"] == 0)]
+    qa_proj = df[(~df.first_date_of_getting_pv.isnull()) & (df["SDK_complete_click"] != 0)]
 
-    qualified_account = df[(df.install_rate <= 1) & (df.install_rate > 0)]
-    qualified_accounts = len(qualified_account)
-    avg_users_in_qa = qualified_account["user"]["len"].mean()
+    qa_num = len(set(qa_proj["project_id"]) - (set(qa_proj["project_id"]) & set(nqa_proj["org_id_organization"])))
+    nqa_num = len(set(nqa_proj["project_id"]))
+    print("Qualified Account : " + str(qa_num))
+    print("None Qualified Account : " + str(nqa_num))
+    print("Install Rate : " + str(round(qa_num / nqa_num, 3) * 100) + "%")
 
-    non_qualified_account = df[df.install_rate == 0]
-    non_qualified_accounts = len(non_qualified_account)
-    avg_users_in_nqa = non_qualified_account["user"]["len"].mean()
+    nqau = set(qa_proj["user"])
+    qau = set(nqa_proj["user"])
 
-    print("Number of qualified accounts :" + str(qualified_accounts))
-    print("Number of non qualified accounts :" + str(non_qualified_accounts))
-    print("Ratio : between qualified and non qualified :" + str(round(qualified_accounts / non_qualified_accounts, 3)*100) + "%")
-    print("Install Rate :" + str(round(qualified_accounts / len(df), 3)*100) + "%")
+    print("Users in qualified account : " + str(len(qau)))
+    print("Users in non qualified account : " + str(len(nqau)))
 
-    print("Avg number of users in qualified accounts : " + str(round(avg_users_in_qa, 2)))
-    print("Avg number of users in non qualified accounts : " + str(round(avg_users_in_nqa, 2)))
+    avg_qau_org = len(qau) / qa_num
+    avg_nqau_org = len(nqau) / nqa_num
 
-    qualified_account_1 = df[(df.install_rate == 1)]
-    qualified_account_1s = len(qualified_account_1)
-    avg_user_in_1qa = qualified_account_1["user"]["len"].mean()
-    std_users_in_1qa = qualified_account_1["user"]["len"].std()
-
-    qualified_account_m = df[(df.install_rate < 1) & (df.install_rate > 0)]
-    qualified_account_ms = len(qualified_account_m)
-    avg_users_in_mqa = qualified_account_m["user"]["len"].mean()
-    # std_users_in_mqa = qualified_account_m["user"]["len"].std()
-
-    print("Number of account User-Install-Rate == 1 : " + str(qualified_account_1s) + " Percentage : " + str(round(qualified_account_1s / qualified_accounts, 3)*100) + "%")
-    print("Number of account User-Install-Rate < 1 : " + str(qualified_account_ms) + " Percentage : " + str(round(qualified_account_ms / qualified_accounts, 3)*100) + "%")
-    print("Avg user on account with UIR == 1 : " + str(avg_user_in_1qa))
-    print("Avg user on account with UIR < 1 : " + str(avg_users_in_mqa))
-
-    qualified_account_org_name = qualified_account["org_name"].drop_duplicates()
-    non_qualified_account_org_name = non_qualified_account["org_name"].drop_duplicates()
-
-    q_duplicate_rate = round(len(qualified_account_org_name[qualified_account_org_name.isin(org_in_db)]) / len(qualified_account_org_name), 3)*100
-    nq_duplicate_rate = round(len(non_qualified_account_org_name[non_qualified_account_org_name.isin(org_in_db)]) / len(non_qualified_account_org_name), 3)*100
-
-    print("Qualified Account Duplicate Rate : " + str(q_duplicate_rate) + "%")
-    print("Non-qualified Account Duplicate Rate : " + str(nq_duplicate_rate) + "%")
-
-
+    print("Avg users in QA : " + str(round(avg_qau_org, 2)))
+    print("Avg users in NQA: " + str(round(avg_nqau_org, 2)))
 
 
 if __name__ == "__main__":
@@ -83,23 +32,15 @@ if __name__ == "__main__":
     # file_name = "SDK选择平台-过去14天.csv"
     # sdk_install_users = pd.read_csv(file_name, encoding="utf-16", sep="\t")
 
-    behavior_file_name = "用户加载SDK.csv"
-    columns = ["date", "user", "page_source", "page", "org_name", "industry", "SDK_platform_view", "SDK_check_click", "SDK_complete_click"]
-    users_behaviors = pd.read_csv(behavior_file_name, encoding="utf-16", sep="\t", names=columns, header=0)
+    behavior_file_name = "SDK 安装－数据源.csv"
+    columns = ["date", "user", "page_source", "SDK_platform_view", "SDK_platform_click", "SDK_check_click", "SDK_complete_click"]
+    users_behaviors = pd.read_csv(behavior_file_name, encoding="utf-16", sep="\t", names=columns, header=0, dtype={"user":str})
+    users_behaviors = users_behaviors[~users_behaviors["user"].isnull()]
 
-    user_org_info_file = "user_org_info.txt"
+    user_org_project_info = pd.read_csv("./user_org_project_info.csv", header=0, dtype={"user_id_project":str})
+    users_in_march = pd.merge(users_behaviors, user_org_project_info, how="left", left_on=["user"], right_on=["user_id_project"])
+    sdk_company_analysis(users_in_march)
 
-    user_org_info = pd.read_csv(user_org_info_file, header=0)
-    org_in_db =  user_org_info["org_name"].drop_duplicates()
-
-    sdk_page_source_analysis(users_behaviors)
-    # sdk_company_analysis(users_behaviors, org_in_db=org_in_db)
-
-    # users_ = users_behaviors[(users_behaviors.SDK_platform_view > 0) & (users_behaviors.SDK_check_click > 0)]
-
-    # print(users_behaviors)
-
-    # events = ["SDK_platform_view", "SDK_check_click", "SDK_complete_click"]
 
 
     ## Funnel Analysis
