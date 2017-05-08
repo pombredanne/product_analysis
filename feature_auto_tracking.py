1df4671433c1ec6ce8568777559cdb700fef7b5a
Roverzon <roverzon@gmail.com>
2017-05-08 17:19:45 +0800

    add basic metrics analysis

--- feature_auto_tracking.py
+++ feature_auto_tracking.py
@@ -1,21 +1,101 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+
+def metrics_sum(sample=pd.DataFrame):
+    grouped_t_yw = sample.groupby(["year", "week"])["id"].agg("count").rename("total")
+    grouped_ah_yw = sample[~(sample.status == "activated")].groupby(["year", "week"])["id"].agg("count").rename("archived")
+    grouped_at_yw = sample[sample.status == "activated"].groupby(["year", "week"])["id"].agg("count").rename("activated")
+
+    print(grouped_ah_yw.describe())
+    print(grouped_at_yw.describe())
+
+    metrics_growth = pd.concat([grouped_at_yw, grouped_ah_yw, grouped_t_yw], axis=1)
+    metrics_growth.plot()
+    plt.show()
+
+    compx = sample[sample.exp_type == "complex"].groupby(["year", "week"])["id"].agg("count").rename("complex")
+    compd = sample[sample.exp_type == "compound"].groupby(["year", "week"])["id"].agg("count").rename("compound")
+    # normal = sample[sample.exp_type == "normal"].groupby(["year", "week"])["id"].agg("count").rename("normal")
+
+    metrics_growth2 = pd.concat([compx, compd], axis=1)
+    metrics_growth2.plot()
+    plt.show()
+
+
+def metrics_type(exp:str):
+    if len(exp.split("/")) > 1:
+        type = "complex"
+    elif len(exp.split("+")) > 1:
+        type = "compound"
+    else:
+        type = "normal"
+    return type
+
+
+def metrics_flexp_sum(sample=pd.DataFrame):
+   grouped_exp = sample.groupby(["exp_type"])["id"].agg("count").rename("count(*)")
+   grouped_pro = round(grouped_exp*100 /np.sum(grouped_exp), 3).rename("pro")
+   result = pd.concat([grouped_exp, grouped_pro], axis=1).sort_values(by="count(*)", ascending=False)
+   # print(result)
+
+   compx_grouped = sample[sample.exp_type == "complex"].groupby(["status"])["id"].agg("count").rename("complex")
+   compx_pro = round(compx_grouped*100 / np.sum(compx_grouped), 2).rename("complex_pro")
+   compd_grouped = sample[sample.exp_type == "compound"].groupby(["status"])["id"].agg("count").rename("compound")
+   compd_pro = round(compd_grouped * 100 / np.sum(compd_grouped), 2).rename("compound_pro")
+   norml_grouped = sample[sample.exp_type == "normal"].groupby(["status"])["id"].agg("count").rename("normal")
+   norml_pro = round(norml_grouped * 100 / np.sum(norml_grouped), 2).rename("normal_pro")
+
+   result = pd.concat([compx_grouped, compx_pro, compd_grouped, compd_pro, norml_grouped, norml_pro], axis=1).fillna(0)
+   print(result)
+
+
+def archive_periods(sample=pd.DataFrame):
+    archived_metrics = sample[sample.status == "archived"]
+    artimedelta = (archived_metrics["updated_at"] - archived_metrics["created_at"]).apply(lambda tdelta : tdelta.total_seconds() / 86400 )
+    archived_metrics = pd.concat([archived_metrics, artimedelta], axis=1)
+
+    print(artimedelta)
+
+
+def parse_metrics_exp(exp=str):
+    els = json.loads(exp)['els'][0]['els']
+    if len(els) > 1:
+        exps = []
+        for e in els:
+            exps.append(e["action"])
+        return "-".join(exps)
+    else:
+        return els[0]["action"]
+
+
+def metrics_exp_sum(sample=pd.DataFrame):
+   sample["exps_abt"] = sample["expression"].map(lambda exp: parse_metrics_exp(exp))
+   # com_metrics = sample[~(sample.exp_type == "normal")].groupby(["exps_abt"])["id"].agg("count").sort_values(ascending=False)
+   # print(com_metrics.head())
+
+   # metrics_type = sample.groupby(["exps_abt"])["id"].agg("count").sort_values(ascending=False)
+   # print(metrics_type.head())
+
+   c_grouped_p = sample[sample.exps_abt.isin(["clck"])].groupby(["project_id", "exps_abt"])["id"].agg("count").rename("clck")
+   i_grouped_p = sample[sample.exps_abt.isin(["imp"])].groupby(["project_id", "exps_abt"])["id"].agg("count").rename("imp")
+   p_grouped_p = sample[sample.exps_abt.isin(["page"])].groupby(["project_id", "exps_abt"])["id"].agg("count").rename("page")
+
+   result = pd.concat([c_grouped_p.describe(), i_grouped_p.describe(), p_grouped_p.describe()], axis=1)
+   print(result)
 
 
 if __name__ == "__main__":
 
-    rules = pd.read_csv("./metrics_rules.csv", parse_dates=["created_at"], dtype={"xpath": str}).dropna()
+    metrics = pd.read_csv("./db_export/metrics.csv", low_memory=False, error_bad_lines=False, parse_dates=["created_at", "updated_at"])
+    metrics = metrics[~(metrics.project_id <= 3)]
+    metrics["exp_type"] = metrics["flatten_expression"].map(lambda exp: metrics_type(exp=exp))
+    metrics["year"] = metrics["created_at"].map(lambda time: time.isocalendar()[0])
+    metrics["week"] = metrics["created_at"].map(lambda time: time.isocalendar()[1])
 
-    ai_grouped = rules.groupby(["ai"]).agg("count")
-
-    print(len(ai_grouped.index))
-
-    # total_rules = len(rules.index)
-    # pattern_tracking = rules[( rules["href"].isnull() & rules["index"].isnull() & rules["content"].isnull())]
-    # ep_tracking = rules[(~rules["href"].isnull() | ~rules["index"].isnull() | ~rules["content"].isnull())]
-    # pos_tracking = ep_tracking[ep_tracking["xpath"].map(lambda xpath: len(xpath.split(",")) > 1)]
-    #
-    # print(pos_tracking)
-
-
-
-
+    # metrics_sum(sample=metrics)
+    # metrics_flexp_sum(sample=metrics)
+    # archive_periods(sample=metrics)
+    metrics_exp_sum(sample=metrics)
--- feature_cohorts.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-if __name__ == "__main__":
-
-    cohorts = pd.read_csv("./metrics_segementation.csv", low_memory=False)
-
-    print(cohorts)
