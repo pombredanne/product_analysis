8bd24b12d244179123de3e2c6959b1eba0ecb8d1
Roverzon <roverzon@gmail.com>
2017-07-18 22:29:16 +0800

    10 metrics and excel writer

--- weekly_data_updator.py
+++ weekly_data_updator.py
@@ -1,80 +1,100 @@
 import xlsxwriter
 import pandas as pd
-import plotly.plotly as py
-import plotly.graph_objs as go
-import matplotlib.pyplot as plt
+from datetime import datetime
 from user_use_behavior import get_active_user
 from user_use_behavior import get_casual_user
 from user_use_behavior import cohort_analysis
 from user_use_behavior import get_login_user
+from user_use_behavior import user_migration
+from user_use_behavior import get_short_return_of_asset
+from user_use_behavior import get_short_return_of_equity
 
+raw = pd.read_csv("./DB/all_0717.csv")
+raw = raw[(raw["week_iso"] < 30)]
 
-raw = pd.read_csv("./DB/all_0710.csv")
-raw = raw[raw["week_iso"] < 30]
-
-
-# active_users_week = get_active_user(raw).groupby("week_iso")["user"].nunique()
-# casual_users_week = get_casual_user(raw).groupby("week_iso")["user"].nunique()
-login_users_week = get_login_user(raw).groupby("week_iso")["user"].nunique()
+cWN = datetime.now().isocalendar()[1]
 
 login_user = get_login_user(raw)
-login_user = login_user.assign(total_time_spent=lambda x : x.avg_duration.astype(float) * x.visits.astype(int))
-srdf = login_user.groupby("week_iso")[["net_income", "capital_expenditure"]].agg("sum").assign(short_return=lambda x: -1* x.net_income / x.capital_expenditure)
-print(srdf)
 
-# print(login_user.groupby("week_iso")["total_time_spent"].sum())
+"""
+Product Value 
+
+No1  Short return of asset ( net_income / capital_expenditure )
+
+Short return of equity ( sum(net_income) / countd(active user )
+
+"""
+
+# short_return_of_equity = login_user.groupby("week_iso").apply(get_short_return_of_equity)
+short_return_of_asset = login_user.groupby("week_iso").apply(get_short_return_of_asset)
 
 
+"""
+    User growth ( Active User, Casual User )
+"""
+
+active_users = get_active_user(login_user).groupby("week_iso")["user_id"].nunique()
+casual_users = get_casual_user(login_user).groupby("week_iso")["user_id"].nunique()
 
 
-#
-#
-# au_reten = cohort_analysis(sample=get_active_user(raw), endP=26)
-# cu_reten = cohort_analysis(sample=get_casual_user(raw), endP=26)
-#
-# users = [active_users_week, casual_users_week, login_users_week]
-# reten = [au_reten, cu_reten]
-#
-# workbook = xlsxwriter.Workbook("weekly_report.xlsx")
-#
-# worksheet = workbook.add_worksheet("user")
-# week_index = active_users_week.index.tolist()
-# worksheet.write_column('A1', week_index)
-#
-# for uid in range(len(users)):
-#     cols = ['B', 'C', 'D']
-#     titles = ["Active User", "Casual User", "Login User"]
-#     worksheet.write_column( cols[uid]+'2', users[uid])
-#     worksheet.write(0, 0, "Week Number")
-#     worksheet.write(0, uid + 1, titles[uid])
-#     chart = workbook.add_chart({'type': 'line'})
-#     chart.set_title({'name': titles[uid]})
-#     chart.add_series({
-#         'values' : '=user!$' + cols[uid] + '2:$' + cols[uid] + '$' + str(len(users[uid]))
-#     })
-#
-#     chart.set_size({'width': 720, 'height': 560})
-#     cols2 = ['F', 'R', 'AD']
-#     worksheet.insert_chart( cols2[uid] + '2' , chart)
-#
-# worksheet = workbook.add_worksheet("1_Week_Retention")
-# week_index = au_reten.index
-# worksheet.write_column('A1', week_index)
-#
-# for uid in range(len(reten)):
-#     cols = ['B', 'C']
-#     titles = ["Active User", "Casual User" ]
-#     worksheet.write_column( cols[uid]+'2', reten[uid][1])
-#     worksheet.write(0,0, "Week Number")
-#     worksheet.write(0,uid + 1 , titles[uid])
-#     chart = workbook.add_chart({'type': 'line'})
-#     chart.set_title({'name': titles[uid]})
-#     chart.add_series({
-#         'values' : '=1_Week_Retention!$' + cols[uid] + '2:$' + cols[uid] + '$' + str(len(users[uid]))
-#     })
-#
-#     chart.set_size({'width': 720, 'height': 560})
-#     cols2 = ['E', 'R', 'Y']
-#     worksheet.insert_chart( cols2[uid] + '2' , chart)
-#
-# workbook.close()
+"""
+    1 Week Retention ( Active User, Casual User )
+"""
+
+au_1Wreten = cohort_analysis(sample=get_active_user(raw), endP=cWN -1)[1]
+cu_1Wreten = cohort_analysis(sample=get_casual_user(raw), endP=cWN -1)[1]
+
+"""
+    1 Week Migration ( Active User, Casual User )
+"""
+
+cu, au, lu = user_migration(login_user, endP=cWN -1)
+
+cu_up_rate = cu["up_to_active"]
+au_down_rate = au["down_to_casual"]
+
+"""
+ new user 1 week retention 
+"""
+new_user_1w = cohort_analysis(sample= login_user[login_user.user_life < 90], endP=cWN -1)[1].fillna(0)
+
+"""
+ Total Time Spent
+
+"""
+
+tts = login_user.assign(total_time_spent=lambda x : round(x.avg_duration.astype(float) * x.visits.astype(int),2) ).groupby("week_iso")["total_time_spent"].sum()
+
+workbook = xlsxwriter.Workbook("weekly_report_WN27" + ".xlsx")
+worksheet = workbook.add_worksheet()
+
+metrics = [ short_return_of_asset, active_users, casual_users,
+            au_1Wreten, cu_1Wreten, cu_up_rate, au_down_rate, new_user_1w, tts ]
+
+metrics_name = ["Short ROE", "Active User", "Casual User",
+                "Active User 1 WR", "Casual User 1 WR",
+                "Casual User Up Rate", "Active User Down Rate", "New User 1 WR", "Total Time Spent"]
+
+rid = 50
+chrA = 65
+
+worksheet.write_row("B" + str(rid), reversed(range(1, cWN)))
+worksheet.write(rid, 0, "week number")
+
+for mid in range(len(metrics)):
+    worksheet.write(rid, 0, str(metrics_name[mid]))
+    worksheet.set_column(firstcol=0, lastcol=0, width=18)
+    worksheet.write_row("B" + str(rid), metrics[mid].sort_index(ascending=False) )
+
+    chart = workbook.add_chart({"type": "line"})
+    chart.add_series({'values': '=Sheet1!$Z$' + str(rid) + ":$B$" + str(rid)})
+
+    rowl = ( mid // 3 ) * 15
+    coll = ( mid % 3 ) * 10
+
+    worksheet.insert_chart( row=rowl + 1, col=coll + 1, chart=chart)
+
+    rid += 1
+    chrA += 1
+
+workbook.close()
